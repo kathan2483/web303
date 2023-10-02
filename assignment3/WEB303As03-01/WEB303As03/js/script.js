78478fd6dd5f2397c6035e19cf0137c61d2a8b65
kathan2483 <68279721+kathan2483@users.noreply.github.com>
2023-10-01 21:09:41 -0400

    Add files via upload

--- assignment3/WEB303As03-01/WEB303As03/js/script.js
+++ assignment3/WEB303As03-01/WEB303As03/js/script.js
@@ -1,25 +1,23 @@
-// Define your getJSONMethod
+// defining method
 function getJSONMethod() {
     $.getJSON('team.json', function(data) {
-        // Remove any existing content in the #team div
+        //deleting old content
         $('#team').empty();
 
-        // Loop through the data array
+        // array
         $.each(data, function(index, teamMember) {
-            // Create HTML elements for name, position, and bio
+            // new elements
             var nameElement = $('<h2>').text(teamMember.name);
             var positionElement = $('<h5>').text(teamMember.position);
             var bioElement = $('<p>').text(teamMember.bio);
 
-            // Append the elements to the #team div
             $('#team').append(nameElement, positionElement, bioElement);
         });
     });
 }
 
-// Define your ajaxMethod
+//ajaxMethod
 function ajaxMethod() {
-    // Display "Loading..." message in the #team div before sending the AJAX request
     $('#team').text('Loading...');
 
     $.ajax({
@@ -27,30 +25,23 @@ function ajaxMethod() {
         url: 'team.json',
         dataType: 'json',
         success: function(data) {
-            // Remove the "Loading..." message
-            $('#team').empty();
+           $('#team').empty();
 
-            // Loop through the data array
             $.each(data, function(index, teamMember) {
-                // Create HTML elements for name, position, and bio
+                
                 var nameElement = $('<h2>').text(teamMember.name);
                 var positionElement = $('<h5>').text(teamMember.position);
                 var bioElement = $('<p>').text(teamMember.bio);
-
-                // Append the elements to the #team div
-                $('#team').append(nameElement, positionElement, bioElement);
+ $('#team').append(nameElement, positionElement, bioElement);
             });
         },
         error: function() {
-            // Display an error message in case of an error
-            $('#team').text('Error: Content could not be retrieved.');
+           $('#team').text('Error: Content could not be retrieved.');
         }
     });
 }
 
-// Call one of the methods in a ready function to run the code
+
 $(document).ready(function() {
-    // You can choose to call either getJSONMethod() or ajaxMethod() here
-    // Example: getJSONMethod();
-    ajaxMethod(); // Since you mentioned it doesn't matter which one you call
+    ajaxMethod();
 });
